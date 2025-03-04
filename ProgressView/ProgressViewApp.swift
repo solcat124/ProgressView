86f4e70f8bcc0bec1854bd4373ebd855ac5f0715
solcat124 <pk80@windbow.com>
2025-03-03 18:20:18 -0800

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Phil Kelly on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProgressViewTests/ProgressViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewTests.swift
+//  ProgressViewTests
+//
+//  Created by Phil Kelly on 3/3/25.
+//
+
+import Testing
+@testable import ProgressView
+
+struct ProgressViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
